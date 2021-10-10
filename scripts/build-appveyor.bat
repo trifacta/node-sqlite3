30934a8f80dd668c6224c1ccc5b5ab5196a7ee44
kewde <kewde@particl.io>
2021-10-10 13:24:34 +0200

    ci: fix & character from getting interpreted and executed

--- scripts/build-appveyor.bat
+++ scripts/build-appveyor.bat
@@ -145,7 +145,7 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 ECHO packaging for node-gyp
 CALL node_modules\.bin\node-pre-gyp package %TOOLSET_ARGS%
 ::make commit message env var shorter
-SET CM=%APPVEYOR_REPO_COMMIT_MESSAGE%
+SET CM="%APPVEYOR_REPO_COMMIT_MESSAGE%"
 IF NOT "%CM%" == "%CM:[publish binary]=%" (ECHO publishing && CALL node_modules\.bin\node-pre-gyp --msvs_version=%msvs_version% publish %TOOLSET_ARGS%) ELSE (ECHO not publishing)
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
